20cd5b8ae7829d976f24ff45437748645c92d178
ollenorelius <norelius.olle@gmail.com>
2016-09-17 14:56:26 +0200

    Padding function
    Added function to pad image with 0 to next power of 2

---
+++ WPT_padPower2.m
@@ -0,0 +1,6 @@
+function image = WPT_padPower2(image)
+imageX = size(image,1)
+imageY = size(image,2)
+padImageX = 2^nextpow2(imageX);
+padImageY = 2^nextpow2(imageY);
+image = padarray(image, [padImageX - imageX, padImageY - imageY], 0, 'post');
