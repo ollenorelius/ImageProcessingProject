a8f1bec808e7a3bb032e9a63275e958b26d67771
Donal <scanlan@student.chalmers.se>
2016-09-23 16:11:27 +0200

    Comp ratio fixed
    Small changes to main

--- WPT_removeSmallest.m
+++ WPT_removeSmallest.m
@@ -1,10 +1,10 @@
-function image = WPT_removeSmallest(image, ratio)
+function image = WPT_removeSmallest(image, ratio, noPadPixels)
 
 imageX = size(image,1);
 imageY = size(image,2);
 
 r = reshape(image,1,[]);
-removeCount = (imageX*imageY)*((ratio-1)/ratio);
+removeCount = (imageX*imageY)*((ratio-1)/ratio)+noPadPixels;
 for i = 1:removeCount
     [~, idx] = min(abs(r));
     r(idx) = 999999;
