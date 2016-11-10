4dcfe157966496683bd607df9f994f12694bdd4b
ollenorelius <norelius.olle@gmail.com>
2016-11-10 13:01:49 +0100

    Fixed truncation
    Now grayscale images work fine.

--- WPT/WPT_removeSmallest.m
+++ WPT/WPT_removeSmallest.m
@@ -3,27 +3,19 @@ function image = WPT_removeSmallest(image, ratio, noPadPixels, ogDim)
 imageX = size(image,1);
 imageY = size(image,2);
 
+nChannels = size(image,3);
+sum = zeros(imageX, imageY);
+for iChan = 1:nChannels
+    sum = sum + image(:,:,iChan).^2;
+end
 
-R = image(:,:,1);
-G = image(:,:,2);
-B = image(:,:,3);
-
-vals = sqrt(R.^2 + G.^2 + B.^2);
+vals = sqrt(sum);
 
 v = reshape(vals,1,[]);
-R = reshape(R,1,[]);
-G = reshape(G,1,[]);
-B = reshape(B,1,[]);
+
 
 removeCount = round((ogDim(1)*ogDim(2))*((ratio-1)/ratio))+noPadPixels;
 fprintf('Removing %i pixels (%i including padding) from image of %i (%i including padded)\n', removeCount-noPadPixels, removeCount, ogDim(1)*ogDim(2), imageX*imageY);
 [~, I] = sort(v);
-R(I(1:removeCount)) = 0;
-G(I(1:removeCount)) = 0;
-B(I(1:removeCount)) = 0;
 
-R = reshape(R, imageX, imageY);
-G = reshape(G, imageX, imageY);
-B = reshape(B, imageX, imageY);
-
-image = cat(3, R, G, B);
+image(I(1:removeCount)) = 0;
